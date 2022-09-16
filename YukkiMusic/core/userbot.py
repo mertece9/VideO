28154f16d44dfbffb37319b3ea3c59aa7715aeb2
mertece9 <112589239+mertece9@users.noreply.github.com>
2022-09-16 11:53:29 +0300

    Update userbot.py

--- YukkiMusic/core/userbot.py
+++ YukkiMusic/core/userbot.py
@@ -57,15 +57,15 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("StarBotKanal")
-                await self.one.join_chat("StarBotKanal")
-                await self.one.join_chat("SohbetGirdap")
+                await self.one.join_chat("sohbetmuhabbetw")
+                await self.one.join_chat("sohbetmuhabbetw")
+                await self.one.join_chat("sohbetmuhabbetw")
             except:
                 pass
             assistants.append(1)
             try:
                 await self.one.send_message(
-                    config.LOG_GROUP_ID, "Assistant Started"
+                    config.LOG_GROUP_ID, "Asistan Çalışmaya Başladı ✅"
                 )
             except:
                 LOGGER(__name__).error(
@@ -88,15 +88,15 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("StarBotKanal")
-                await self.two.join_chat("StarBotKanal")
-                await self.two.join_chat("SohbetGirdap")
+                await self.two.join_chat("sohbetmuhabbetw")
+                await self.two.join_chat("sohbetmuhabbetw")
+                await self.two.join_chat("sohbetmuhabbetw")
             except:
                 pass
             assistants.append(2)
             try:
                 await self.two.send_message(
-                    config.LOG_GROUP_ID, "Assistant Started"
+                    config.LOG_GROUP_ID, "Asistan Çalışmaya Başladı ✅"
                 )
             except:
                 LOGGER(__name__).error(
@@ -119,15 +119,15 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("StarBotKanal")
-                await self.three.join_chat("StarBotKanal")
-                await self.three.join_chat("SohbetGirdap")
+                await self.three.join_chat("sohbetmuhabbetw")
+                await self.three.join_chat("sohbetmuhabbetw")
+                await self.three.join_chat("sohbetmuhabbetw")
             except:
                 pass
             assistants.append(3)
             try:
                 await self.three.send_message(
-                    config.LOG_GROUP_ID, "Assistant Started"
+                    config.LOG_GROUP_ID, "Asistan Çalışmaya Başladı ✅"
                 )
             except:
                 LOGGER(__name__).error(
@@ -150,15 +150,15 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("StarBotKanal")
-                await self.four.join_chat("StarBotKanal")
-                await self.four.join_chat("SohbetGirdap")
+                await self.four.join_chat("sohbetmuhabbetw")
+                await self.four.join_chat("sohbetmuhabbetw")
+                await self.four.join_chat("sohbetmuhabbetw")
             except:
                 pass
             assistants.append(4)
             try:
                 await self.four.send_message(
-                    config.LOG_GROUP_ID, "Assistant Started"
+                    config.LOG_GROUP_ID, "Asistan Çalışmaya Başladı ✅"
                 )
             except:
                 LOGGER(__name__).error(
@@ -181,15 +181,15 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("StarBotKanal")
-                await self.five.join_chat("StarBotKanal")
-                await self.five.join_chat("SohbetGirdap")
+                await self.five.join_chat("sohbetmuhabbetw")
+                await self.five.join_chat("sohbetmuhabbetw")
+                await self.five.join_chat("sohbetmuhabbetw")
             except:
                 pass
             assistants.append(5)
             try:
                 await self.five.send_message(
-                    config.LOG_GROUP_ID, "Assistant Started"
+                    config.LOG_GROUP_ID, "Asistan Çalışmaya Başladı ✅"
                 )
             except:
                 LOGGER(__name__).error(
